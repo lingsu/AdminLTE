206737e106b5e9543e54dc63320ff42e00749ab0
xins Bin <q25a25q@live.com>
2015-11-17 21:58:15 +0800

    修改用户管理

--- LyuAdmin.Application/Roles/Dto/RoleDto.cs
+++ LyuAdmin.Application/Roles/Dto/RoleDto.cs
@@ -1,13 +1,56 @@
-﻿using Abp.AutoMapper;
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Abp.Runtime.Validation;
+using Abp.Extensions;
 using LyuAdmin.Authorization.Roles;
+
 
 namespace LyuAdmin.Roles.Dto
 {
-    [AutoMap(typeof(Role))]
-    public class RoleDto
+
+    /// <summary>
+    /// 角色管理
+    /// </summary>
+
+  [AutoMap(typeof(Role))]
+    public class RoleDto : EntityDto, IValidate
     {
-        public virtual string Name { get; set; }
-        public virtual string DisplayName { get; set; }
-        public virtual bool IsDefault { get; set; }
+        /// <summary>
+        /// string
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// string
+        /// </summary>
+        [Required]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// bool
+        /// </summary>
+        public bool IsStatic { get; set; }
+
+        /// <summary>
+        /// bool
+        /// </summary>
+        public bool IsDefault { get; set; }
+
+        /// <summary>
+        /// bool
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// DateTime
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
     }
 }
+
+
